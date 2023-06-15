de408d1f799ed43e419cab614870e44869240fe6
fancop <113712008+fancop@users.noreply.github.com>
2023-06-15 17:12:58 +0300

    Add files via upload

--- analizatorteksta.py
+++ analizatorteksta.py
@@ -1,3 +1,7 @@
+from typing import NoReturn
+from string import punctuation
+
+
 """
 скачать текст
 загрузить ТХТ файл
@@ -6,17 +10,18 @@
 """
 
 class TextAnalyzer:
-    def __init__(self, file_name="text.txt", mode="r", encoding="UTF-8"):
+    def __init__(self, file_name="text.txt", mode="r", encoding="UTF-8") -> None:
         if file_name is None:
             raise Exception("Не указан файл для анализа!")
         self.file_name = file_name
         self.mode = mode
         self.encoding = encoding
         self.read_file()
+        self.check_empty_file()
         self.prepare_text()
         self.print_text()
 
-    def read_file(self):
+    def read_file(self) -> None | NoReturn:
         """ Пытается открыть файл и считать его в строку """
         try:
             with open(self.file_name, self.mode, encoding=self.encoding) as file:
@@ -24,16 +29,29 @@ class TextAnalyzer:
                 self.text = self.content.read()
         except FileNotFoundError:
             raise Exception(f"Файл {self.file_name} не найден!")
-        
+
+    def check_empty_file(self) -> None | NoReturn:
+        """ проверяет пустой ли файл """
         if not self.text:
-            raise Exception("Файл пустой!")
+            raise RuntimeError(f"Файл {self.file_name} пустой!")
 
     def prepare_text(self):
-        """ Приводит текст к нижнему регистру """
+        """ Приводит текст к нижнему регистру и убирает все лишние знаки препинания """
         self.text = self.text.lower()
+        self.words_unclean = self.text.split()
+        self.words_clean = []
+        for i in range(len(self.words_unclean)):
+            for s in punctuation + "—":
+                new_word = self.words_unclean[i].replace(s, "")
+                if new_word:
+                    self.words_clean.append(new_word)
+        self.words_clean = list(filter(None, self.words_clean))
+        self.words_clean.append(new_word)
+        
 
-    def print_text(self):
+    def print_text(self) -> None:
         """ Выводит строку текста на экран """
-        print(self.text)
+        print(self.words_clean)
+        print(f"В этом тексте {len(self.words_clean)} слов")
 
 TextAnalyzer()
