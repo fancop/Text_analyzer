267d6388dbbd6d6833b01b758f520acc36ec95e8
fancop <113712008+fancop@users.noreply.github.com>
2023-09-20 18:21:58 +0300

    Add files via upload

--- TextAnalyzerGUI.py
+++ TextAnalyzerGUI.py
@@ -1,8 +1,9 @@
 import tkinter as tk
-from tkinter import colorchooser
+from tkinter import colorchooser, filedialog
 import TextAnalyzer
 
 color = ""  # Variable to store the selected color
+file_path = ""
 
 def show_color_picker():
     global color
@@ -10,15 +11,21 @@ def show_color_picker():
     if color[1]:
         print("Выбранный цвет:", color[1])
 
+def select_file():
+    global file_path
+    file_path = filedialog.askopenfilename(title="Выберите текстовый файл", filetypes=[("Text Files", "*.txt")])
+    if file_path:
+        print("Выбранный файл:", file_path)
+
 def run():
     pos_list = []
-    if noun_var.get():  # If Noun checkbox is selected
-        pos_list.append('NOUN')
-    if verb_var.get():  # If Verb checkbox is selected
-        pos_list.append('VERB')
+    if noun_var.get():
+        pos_list.append("NOUN")
+    if verb_var.get():
+        pos_list.append("VERB")
     
     TextAnalyzer.TextAnalyzer(
-        file_name='text.txt',
+        file_name=file_path,
         pos_list=pos_list,
         num=int(entry.get()),
         background=color[1],
@@ -27,20 +34,22 @@ def run():
     )
 
 window = tk.Tk()
-label = tk.Label(window, text="Сколько слов должно быть", font=("Script", 19))
+label = tk.Label(window, text="Сколько слов должно быть", font=("Bahnschrift", 19))
 noun_var = tk.BooleanVar()  # Variable to store the state of Noun checkbox
 verb_var = tk.BooleanVar()  # Variable to store the state of Verb checkbox
-cb_noun = tk.Checkbutton(window, text="Включить Существительные", font=("Script", 19), variable=noun_var)
-cb_verb = tk.Checkbutton(window, text="Включить Глаголы", font=("Script", 19), variable=verb_var)
-label2 = tk.Label(window, text="Ширина:", font=("Script", 19))
-label3 = tk.Label(window, text="Высота:", font=("Script", 19))
-button = tk.Button(window, font=("Script", 17), text="сделать вордклауд", command=run)
-button2 = tk.Button(window, font=("Script", 17), text="выбрать цвет", command=show_color_picker)
-entry = tk.Entry(window, font=("Script", 18))
-entry2 = tk.Entry(window, font=("Script", 18))
-entry3 = tk.Entry(window, font=("Script", 18))
+cb_noun = tk.Checkbutton(window, text="Включить Существительные", font=("Bahnschrift", 19), variable=noun_var)
+cb_verb = tk.Checkbutton(window, text="Включить Глаголы", font=("Bahnschrift", 19), variable=verb_var)
+label2 = tk.Label(window, text="Ширина:", font=("Bahnschrift", 19))
+label3 = tk.Label(window, text="Высота:", font=("Bahnschrift", 19))
+button = tk.Button(window, font=("Bahnschrift", 17), text="сделать вордклауд", command=run)
+button2 = tk.Button(window, font=("Bahnschrift", 17), text="выбрать цвет", command=show_color_picker)
+button3 = tk.Button(window, font=("Bahnschrift", 17), text="выбрать файл", command=select_file)
+entry = tk.Entry(window, font=("Bahnschrift", 18))
+entry2 = tk.Entry(window, font=("Bahnschrift", 18))
+entry3 = tk.Entry(window, font=("Bahnschrift", 18))
 
 label.pack(anchor="nw", padx=6, pady=6)
+button3.pack(anchor="nw", padx=6, pady=6)
 entry.pack(anchor="nw", padx=6, pady=6)
 button.pack(anchor="nw", padx=6, pady=6)
 button2.pack(anchor="nw", padx=6, pady=6)
